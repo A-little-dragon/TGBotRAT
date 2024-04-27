4db3b9e4260ef2807e01c7a843cc0ad8adf7aad4
A-little-dragon <2624276393@qq.com>
2024-04-27 15:06:14 +0800

    创建项目基本结构框架

---
+++ TGBot_shell.py
@@ -0,0 +1,19 @@
+import requests
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S")
+
+
+class TGBotShell:
+    def __init__(self,TGBotToken):
+        self.TGBotToken = TGBotToken
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    NewTgbot = TGBotShell(TGBotToken="6404733258:AAGqbh-GgduFjZaE0-fH3WVUJBROrvdMFmA").run()
